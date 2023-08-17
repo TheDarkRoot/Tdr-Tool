b4e9f541f06f1ff977340aea045e6ca18da60838
TheDarkRoot <leejunfan43@gmail.com>
2023-08-18 00:43:06 +0300

    Tdr-Tool
    TheDarkRoot tool pack.

--- Tdr-Tool.sh
+++ Tdr-Tool.sh
@@ -40,44 +40,47 @@ $CC    #    #    # #    #  #####  #    #    # #    # #
 $CC    #    #    # #####          #    #    # #    # #
 $CC    #    #    # #   #          #    #    # #    # #
 $CC    #    #####  #    #         #     ####   ####  ######
-$YY ####################[$GG TheDarkRoot$YY ]####################\n
-$CC =======================================================\n
-$CC ┌⊸⟜┬─⊸ [\033[33;1mTheDarkRoot Repositories:$CC]\n │  │
-$CC │  ├─┬─⊸ [\033[0;1m1$CC]$GG AnonSMS
-$CC │  │ └─⊸ [\033[33;1mi$CC]$GG Anonymous SMS sending tool.
-$CC │  ├─┬─⊸ [\033[0;1m2$CC]$GG Hasher
-$CC │  │ └─⊸ [\033[33;1mi$CC]$GG This is a Hash Cracker.
-$CC │  ├─┬─⊸ [\033[0;1m3$CC]$GG Hashgen
-$CC │  │ └─⊸ [\033[33;1mi$CC]$GG Generate more 39 type hash.
-$CC │  ├─┬─⊸ [\033[0;1m4$CC]$GG Terpack
-$CC │  │ └─⊸ [\033[33;1mi$CC]$GG TheDarkRoot termux package installer.
-$CC │  ├─┬─⊸ [\033[0;1m5$CC]$GG Tertest
-$CC │  │ └─⊸ [\033[33;1mi$CC]$GG Termux internet speed test.
-$CC │  ├─┬─⊸ [\033[0;1m6$CC]$GG Tertext
-$CC │  │ └─⊸ [\033[33;1mi$CC]$GG Program for creating words from letters.
-$CC │  └─┬─⊸ [\033[0;1m7$CC]$GG UserID
-$CC │    └─⊸ [\033[33;1mi$CC]$GG Search usernames on social media.\n$CC │
-$CC └⊸⟜┬─⊸ [\033[33;1mTermux Settings:$CC]\n    │
-$CC    ├─┬─⊸ [\033[0;1mU$CC]$GG Update
-$CC    │ └─⊸ [\033[33;1mi$CC]$GG Termux update.
-$CC    ├─┬─⊸ [\033[0;1mP$CC]$GG ParrotOS-T
-$CC    │ └─⊸ [\033[33;1mi$CC]$GG Parrot OS theme for Termux.
-$CC    ├─┬─⊸ [\033[0;1mT$CC]$GG TheDarkRoot-T
-$CC    │ └─⊸ [\033[33;1mi$CC]$GG TheDarkRoot theme for Termux.
-$CC    ├─┬─⊸ [\033[0;1mK$CC]$GG Terkey
-$CC    │ └─⊸ [\033[33;1mi$CC]$GG Utility to add direction keys to Termux.
-$CC    └─┬─⊸ [\033[0;1mE$CC]$GG Exit
-$CC      └─⊸ [\033[33;1mi$CC]$GG Tdr-Tool exit.$YY\n"
-read -p " [*] Program Number: " pn
+$YY ####################[$GG TheDarkRoot$YY ]####################
+$CC =======================================================
+$CC ┌⊸⟜┬───⊸ [$MM TheDarkRoot Repositories:$CC ]
+$CC │  ├─┬─⊸ [$YY 1$CC ]$BB ⟫$RR AnonSMS
+$CC │  │ └─⊸ [$YY *$CC ]$GG Anonymous SMS sending tool.
+$CC │  ├─┬─⊸ [$YY 2$CC ]$BB ⟫$RR Hasher
+$CC │  │ └─⊸ [$YY *$CC ]$GG This is a Hash Cracker.
+$CC │  ├─┬─⊸ [$YY 3$CC ]$BB ⟫$RR Hashgen
+$CC │  │ └─⊸ [$YY *$CC ]$GG Generate more 39 type hash.
+$CC │  ├─┬─⊸ [$YY 4$CC ]$BB ⟫$RR Terpack
+$CC │  │ └─⊸ [$YY *$CC ]$GG TheDarkRoot termux package installer.
+$CC │  ├─┬─⊸ [$YY 5$CC ]$BB ⟫$RR Tertest
+$CC │  │ └─⊸ [$YY *$CC ]$GG Termux internet speed test.
+$CC │  ├─┬─⊸ [$YY 6$CC ]$BB ⟫$RR Tertext
+$CC │  │ └─⊸ [$YY *$CC ]$GG Program for creating words from letters.
+$CC │  ├─┬─⊸ [$YY 7$CC ]$BB ⟫$RR UserID
+$CC │  │ └─⊸ [$YY *$CC ]$GG Search usernames on social media.
+$CC │  └─┬─⊸ [$YY X$CC ]$BB ⟫$RR X-Project
+$CC │    └─⊸ [$YY *$CC ]$GG Code in the trial period.
+$CC └⊸⟜┬───⊸ [$MM Termux Settings:$CC ]
+$CC    ├─┬─⊸ [$YY U$CC ]$BB ⟫$RR Update
+$CC    │ └─⊸ [$YY *$CC ]$GG Termux update.
+$CC    ├─┬─⊸ [$YY P$CC ]$BB ⟫$RR ParrotOS-T
+$CC    │ └─⊸ [$YY *$CC ]$GG Parrot OS theme for Termux.
+$CC    ├─┬─⊸ [$YY T$CC ]$BB ⟫$RR TheDarkRoot-T
+$CC    │ └─⊸ [$YY *$CC ]$GG TheDarkRoot theme for Termux.
+$CC    ├─┬─⊸ [$YY K$CC ]$BB ⟫$RR Terkey
+$CC    │ └─⊸ [$YY *$CC ]$GG Utility to add direction keys to Termux.
+$CC    └─┬─⊸ [$YY Q$CC ]$BB ⟫$RR Exit
+$CC      └─⊸ [$YY *$CC ]$GG Tdr-Tool exit.\n"
+read -p " $(echo -e " ${CC}[${YY}*${CC}]${MM} Program Number: ${YY}")" pn
 if [[ $pn == U || $pn == u ]]; then
-clear;echo -e "$CC\n [$YY↓$CC]$GG Updating...\n";apt update;apt upgrade -y;clear;
-echo -e "$CC [$YY*$CC]$GG Termux setup storage...$YY";
-( rm -rf storage;termux-setup-storage; ) &> /dev/null & spin;
-echo -e "$CC [$YY*$CC]$GG Pkg installing...$YY";
-( pkg install ruby -y;pkg install git -y;pkg install python -y;pkg install python2 -y;pkg install python3 -y;pkg install php -y;pkg install zip -y;pkg install unzip -y;pkg install cowsay -y;pkg install figlet;pkg install wget -y;pkg install curl -y;pkg install vim -y;pkg install proot -y;pkg install crunch;pkg install neofetch;pkg install nodejs -y;pkg install nodejs-lts -y;pkg install nano;pkg install cmatrix;pkg install toilet -y;pkg install zsh -y;pkg install sl;pkg install tmate -y;apt install bash -y;pkg install tur-repo;pkg install zphisher -y;pkg install termux-api;pkg install openssh -y;apt install openssl-tool -y; ) &> /dev/null & spin;
-echo -e "$CC [$YY*$CC]$GG Pip installing...$YY";
-( gem install lolcat;pip install --upgrade pip;pip2 install --upgrade pip;pip install bs4;pip2 install bs4;pip install requests;pip2 install requests;pip install mechanize;pip2 install mechanize;pip install passlib;pip2 install passlib;pip install progressbar;pip2 install progressbar;pip install pillow;pip2 install pillow;pip install termcolor speedtest-cli;npm install readline-sync;npm install;npm install --global speed-test;python -m pip install requests; ) &> /dev/null & spin;
-echo -e "$CC [$YY*$CC]$GG Tdr-Tool updating...$YY";
+clear;echo -e "$CC\n [$YY↓$CC]$GG Updating...\n";apt update -y;apt upgrade -y;clear;
+#Termux Packages Installing
+echo -e "$CC [$YY*$CC]$GG Packages Installing...";
+( pkg install ruby git python python2 python3 python-pip php zip unzip cowsay figlet wget curl vim proot crunch neofetch nano cmatrix toilet zsh sl tmate bash tor privoxy -y;pkg install termux-api termux-tools play-audio mpv openssh openssl-tool crunch -y; ) &> /dev/null & spin;
+#Termux Tools Installing
+echo -e "$CC [$YY*$CC]$GG Tools Installing...";
+( gem install lolcat;pip3 install --upgrade pip;pip3 install bs4 requests mechanize passlib progressbar2 pillow termcolor speedtest speedtest-cli;pkg install nodejs -y;pkg install nodejs-lts -y;npm install readline-sync;npm install;npm install --global speed-test; ) &> /dev/null & spin;
+#Termux Tdr-Tool Updating
+echo -e "$CC [$YY*$CC]$GG Tdr-Tool Updating...$YY";
 ( cd ~/Tdr-Tool/;curl https://raw.githubusercontent.com/TheDarkRoot/Tdr-Tool/master/Tdr-Tool.sh -o Tdr-Tool.sh; ) &> /dev/null & spin;
 bash Tdr-Tool.sh
 
@@ -102,8 +105,8 @@ elif [[ $pn == K || $pn == k ]]; then
 	rm -rf Terkey.sh
         bash Tdr-Tool.sh
 
-elif [[ $pn == E || $pn == e ]]; then
-        echo -e "\n$CC [$YY*$CC]$RR Good bye...$YY\n";
+elif [[ $pn == Q || $pn == q ]]; then
+        echo -e "\n $CC [$YY*$CC]$RR Good bye...";
 		sleep 0;exit;
 
 elif [[ $pn == 1 || $pn == 01 ]]; then
@@ -148,8 +151,8 @@ elif [[ $pn == 7 || $pn == 07 ]]; then
         bash Tdr-Tool.sh
 
 else
-	echo -e '\n \033[33;1m[\033[31;1mX\033[33;1m]\033[31;1m Invalid action.\n'	
-	sleep 2
+	echo -e "\n  ${YY}[${RR}X${YY}]${RR} Invalid action."	
+	sleep 1
 	clear
 	bash Tdr-Tool.sh
 fi
