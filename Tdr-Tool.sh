4ff6514c2e242673a578754b7fd9bb3116f8b083
TheDarkRoot <leejunfan43@gmail.com>
2026-06-01 23:19:50 +0300

    Tdr
    Tdr

--- Tdr-Tool.sh
+++ Tdr-Tool.sh
@@ -81,7 +81,7 @@ read -p " $(echo -e " ${CC}[${YY}»${CC}]${MM} Program Number: ${YY}")" pn
 	
 	if [[ $pn == U || $pn == u ]]; then
 	#Termux Update
-	echo -e ( apt update -y;apt upgrade -y; ) &> /dev/null & spin; " $CC\n [$YY↓$CC]$GG Updating..." "$WW⟫$GG Complete."
+	( apt update -y;apt upgrade -y; ) &> /dev/null & spin " $CC [$YY↓$CC]$GG Updating..." "$WW⟫$GG Complete."
 	#Termux Packages Installing
 	echo -e " $CC [$YY↓$CC]$GG Packages Installing...";
 	( pkg install ruby git python python2 python3 python-pip php zip unzip cowsay figlet wget curl vim proot crunch neofetch nano cmatrix toilet zsh sl tmate bash tor privoxy -y;pkg install termux-api termux-tools play-audio mpv openssh openssl-tool crunch -y; ) &> /dev/null & spin; 
